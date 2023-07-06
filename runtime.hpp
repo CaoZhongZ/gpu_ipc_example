5f0aaf912295f260ee591263e61583bf6a6aab28
Cao Zhong Z <zhong.z.cao@intel.com>
2023-07-06 19:28:25 +0800

    support more devices
    Signed-off-by: Cao Zhong Z <zhong.z.cao@intel.com>

--- runtime.hpp
+++ runtime.hpp
@@ -37,17 +37,31 @@ sycl::queue getQueue() {
 sycl::queue currentQueue(int ndev, int nsub) {
   switch(ndev) {
   case 0:
-    if (nsub == 0)
-      return getQueue<0,0>();
-    else
-      return getQueue<0,1>();
-    break;
+    if (nsub == 0) return getQueue<0,0>();
+    else return getQueue<0,1>();
   case 1:
-    if (nsub == 0)
-      return getQueue<1,0>();
-    else
-      return getQueue<1,1>();
-    break;
+    if (nsub == 0) return getQueue<1,0>();
+    else return getQueue<1,1>();
+  case 2:
+    if (nsub == 0) return getQueue<2,0>();
+    else return getQueue<2,1>();
+  case 3:
+    if (nsub == 0) return getQueue<3,0>();
+    else return getQueue<3,1>();
+  }
+  throw std::exception();
+}
+
+sycl::device currentDevice(int ndev) {
+  switch(ndev) {
+  case 0:
+    return getDevice<0>();
+  case 1:
+    return getDevice<1>();
+  case 2:
+    return getDevice<2>();
+  case 3:
+    return getDevice<3>();
   }
   throw std::exception();
 }
@@ -55,17 +69,17 @@ sycl::queue currentQueue(int ndev, int nsub) {
 sycl::device currentSubDevice(int ndev, int nsub) {
   switch(ndev) {
   case 0:
-    if (nsub == 0)
-      return getSubDevice<0,0>();
-    else
-      return getSubDevice<0,1>();
-    break;
+    if (nsub == 0) return getSubDevice<0,0>();
+    else return getSubDevice<0,1>();
   case 1:
-    if (nsub == 0)
-      return getSubDevice<1,0>();
-    else
-      return getSubDevice<1,1>();
-    break;
+    if (nsub == 0) return getSubDevice<1,0>();
+    else return getSubDevice<1,1>();
+  case 2:
+    if (nsub == 0) return getSubDevice<2,0>();
+    else return getSubDevice<2,1>();
+  case 3:
+    if (nsub == 0) return getSubDevice<3,0>();
+    else return getSubDevice<3,1>();
   }
   throw std::exception();
 }
