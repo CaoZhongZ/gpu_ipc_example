ad7c2b595127dbddcee1ee510ea342a3e048d335
Cao, Zhong Z <zhong.z.cao@intel.com>
2023-07-05 19:23:36 -0700

    Correct config NEO
    Signed-off-by: Cao, Zhong Z <zhong.z.cao@intel.com>

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 export ONEAPI_DEVICE_SELECTOR=level_zero:gpu
 export PATH=../ccl/release/_install/bin:$PATH
 export LD_LIBRARY_PATH=../ccl/release/_install/lib:$LD_LIBRARY_PATH
+export NEOReadDebugKeys=1
 export EnableImplicitScaling=0
 
 mpirun -np 1 ./fill_remote -c 16 -t fp16 : -np 1 ./fill_remote -c 16 -t fp16
