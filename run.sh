8ef29541f07683bdf50ab5c5630c4cb533f6a61f
Cao, Zhong Z <zhong.z.cao@intel.com>
2023-07-10 23:29:11 -0700

    clean up script
    Signed-off-by: Cao, Zhong Z <zhong.z.cao@intel.com>

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ export NEOReadDebugKeys=1
 export EnableImplicitScaling=0
 
 # mpirun -np 1 ./fill_remote -c 16 -t fp16 : -np 1 ./fill_remote -c 16 -t fp16
-trace_cmd="unitrace --demangle --chrome-device-timeline --chrome-call-logging --chrome-no-thread-on-device --chrome-no-engine-on-device"
+# trace_cmd="unitrace --demangle --chrome-device-timeline --chrome-call-logging --chrome-no-thread-on-device --chrome-no-engine-on-device"
+trace_cmd=""
 # $trace_cmd mpirun -np 2 ./fill_remote -c 16 -t fp16
 mpirun -np 2 $trace_cmd ./fill_remote -c 16 -t fp16
