3898a6cffaf5e0eb841f9e2c4b8d9ed510ac55a6
Cao, Zhong Z <zhong.z.cao@intel.com>
2023-07-10 23:30:13 -0700

    Use 8 node as testing
    Signed-off-by: Cao, Zhong Z <zhong.z.cao@intel.com>

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ export EnableImplicitScaling=0
 # trace_cmd="unitrace --demangle --chrome-device-timeline --chrome-call-logging --chrome-no-thread-on-device --chrome-no-engine-on-device"
 trace_cmd=""
 # $trace_cmd mpirun -np 2 ./fill_remote -c 16 -t fp16
-mpirun -np 2 $trace_cmd ./fill_remote -c 16 -t fp16
+mpirun -np 8 $trace_cmd ./fill_remote -c 16 -t fp16
