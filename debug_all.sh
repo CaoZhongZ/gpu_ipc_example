c3ccc3074781ade37ff4331699d5e85f2bc969b2
Cao, Zhong Z <zhong.z.cao@intel.com>
2023-07-11 01:14:54 -0700

    Shrink node number
    Signed-off-by: Cao, Zhong Z <zhong.z.cao@intel.com>

--- debug_all.sh
+++ debug_all.sh
@@ -4,5 +4,10 @@ export PATH=../ccl/release/_install/bin:$PATH
 export LD_LIBRARY_PATH=../ccl/release/_install/lib:$LD_LIBRARY_PATH
 export NEOReadDebugKeys=1
 export EnableImplicitScaling=0
+export NEO_BIN=/home/caozhong/Workspace/neo/bin
+export NEO_LIB=/home/caozhong/Workspace/neo/lib
+export PATH=$NEO_BIN:$PATH
+export LD_LIBRARY_PATH=$(pwd)/_install/lib:$NEO_LIB:$LD_LIBRARY_PATH
+export OCL_ICD_VENDORS=/home/caozhong/Workspace/neo/etc/OpenCL/vendors/intel.icd
 
-mpirun -disable-auto-cleanup -np 7 ./fill_remote -c 16 -t fp16 : -np 1 gdbserver :44555 ./fill_remote -c 16 -t fp16
+mpirun -disable-auto-cleanup -np 3 ./fill_remote -c 16 -t fp16 : -np 1 gdbserver :44555 ./fill_remote -c 16 -t fp16
