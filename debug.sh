d83fd0ee428b7ad7afdac11b8b2039efbc81a704
Cao, Zhong Z <zhong.z.cao@intel.com>
2023-08-13 23:19:50 -0700

    Update script
    Signed-off-by: Cao, Zhong Z <zhong.z.cao@intel.com>

--- debug.sh
+++ debug.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export FI_PROVIDER=tcp
 export ONEAPI_DEVICE_SELECTOR=level_zero:gpu
-export PATH=/home/caozhong/Workspace/ccl/release/_install/bin:$PATH
-export LD_LIBRARY_PATH=/home/caozhong/Workspace/ccl/release/_install/lib:$LD_LIBRARY_PATH
+# export PATH=/home/caozhong/Workspace/ccl/release/_install/bin:$PATH
+# export LD_LIBRARY_PATH=/home/caozhong/Workspace/ccl/release/_install/lib:$LD_LIBRARY_PATH
 
 mpirun -disable-auto-cleanup \
   -np 1 gdbserver :44555 ./fill_remote $@ : \
