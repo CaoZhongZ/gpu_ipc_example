c48074b4aecde40c6c53a6b8186e6efff2508cb1
Cao, Zhong Z <zhong.z.cao@intel.com>
2023-08-17 07:50:42 -0700

    Fix bugs
    Signed-off-by: Cao, Zhong Z <zhong.z.cao@intel.com>

--- linearize.cpp
+++ linearize.cpp
@@ -1,19 +1,27 @@
-#include <sycl/sycl.cpp>
+#include <sycl/sycl.hpp>
 #include "sycl_misc.hpp"
 
 int main(int argc, char *argv[]) {
   auto queue = currentQueue(0, 0);
 
   struct coord {
-    size_t y, x;
+    size_t y;
+    size_t x;
   };
 
-  coord* buffer = sycl::malloc_shared(2048 * sizeof(coord), queue);
+  coord* buf = (coord *)sycl::malloc_shared(2048 * sizeof(coord), queue);
+  queue.memset(buf, 1, 2048 * sizeof(coord));
 
-  queue.submit([&](sycl::handler &cgh) {
+  auto e = queue.submit([&](sycl::handler &cgh) {
       cgh.parallel_for(sycl::nd_range<2>({2, 128}, {2, 64}),
           [=](sycl::nd_item<2> pos) {
-            buffer[pos.get_global_linear_id()] = {pos.get_local_id(0), pos.get_local_id(1)};
+            buf[pos.get_global_linear_id()] = {pos.get_local_id(0), pos.get_local_id(1)};
           });
   });
+
+  e.wait();
+
+  for (int i = 0; i < 256; ++ i)
+    printf("(%ld, %ld) ", buf[i].y, buf[i].x);
+  printf("\n");
 }
