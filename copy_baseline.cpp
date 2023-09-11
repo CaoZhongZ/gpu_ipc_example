9d7825c9f4633f5df9e77f4db3a87108e9438aeb
Cao, Zhong Z <zhong.z.cao@intel.com>
2023-09-11 16:49:32 -0700

    Try different strategy
    Signed-off-by: Cao, Zhong Z <zhong.z.cao@intel.com>

--- copy_baseline.cpp
+++ copy_baseline.cpp
@@ -4,16 +4,40 @@
 #include "cxxopts.hpp"
 #include "sycl_misc.hpp"
 
-template <typename T, int lane_v>
+template <typename T, int Unroll>
+struct power_copy {
+  static inline void run(sycl::nd_item<1> pos, T* dst, const T* src, size_t elems) {
+    for (size_t off = pos.get_global_id(0) * Unroll;
+        off < elems; off += pos.get_global_range(0) * Unroll) {
+#     pragma unroll
+      for (int i = 0; i < Unroll; ++ i)
+        if (off + i < elems)
+          dst[off + i] = src[off + i];
+    }
+  }
+};
+
+template <typename T, int Unroll>
+struct jump_copy {
+  static inline void run(sycl::nd_item<1> pos, T* dst, const T* src, size_t elems) {
+    for (size_t off = pos.get_global_id(0);
+        off  < elems; off += pos.get_global_range(0) * Unroll) {
+#     pragma unroll
+      for (int i = 0; i < Unroll; ++ i) {
+        auto i_off = off + pos.get_global_range(0) * i;
+        if (i_off < elems)
+          dst[off + pos.get_global_range(0) * i] = src[off + pos.get_global_range(0) * i];
+      }
+    }
+  }
+};
+
+template <typename T, int lane_v, int Unroll, template <typename, int> class copy_policy>
 struct copy_persist {
   using v_T = sycl::vec<T, lane_v/sizeof(T)>;
 
-  void operator() [[sycl::reqd_sub_group_size(16)]] (
-      sycl::nd_item<1> pos) const {
-    for (size_t off = pos.get_global_id(0);
-        off < v_elems; off += pos.get_global_range(0)) {
-      dst[off] = src[off];
-    }
+  void operator() [[sycl::reqd_sub_group_size(16)]] (sycl::nd_item<1> pos) const {
+    copy_policy<v_T, Unroll>::run(pos, dst, src, v_elems);
   }
 
   copy_persist(T* dst, const T* src, size_t elems) :
@@ -21,13 +45,19 @@ struct copy_persist {
     dst(reinterpret_cast<v_T *>(dst)),
     v_elems(elems/v_T::size()) {}
 
-  static sycl::event launch(T* dst, const T* src, size_t nelems) {
-    constexpr size_t max_group = 512;
-    constexpr size_t local_size = 128;
+  static sycl::event launch(T* dst, const T* src, size_t nelems, size_t max_group =64, size_t local_size = 1024) {
+    auto v_nelems = nelems / v_T::size();
 
-    size_t group_size = (nelems/v_T::size() + local_size -1)/local_size;
-    group_size = std::min(group_size, max_group);
-    size_t global_size = group_size * local_size;
+    if (v_nelems % Unroll != 0)
+      throw std::logic_error("Unroll can't be satisfied");
+
+    v_nelems /= Unroll;
+
+    size_t required_groups = (v_nelems + local_size -1)/local_size;
+    auto group_num = std::min(required_groups, max_group);
+    size_t global_size = group_num * local_size;
+
+    printf("Launch copy_persist (%zu, %zu) with unroll %d\n", group_num, local_size, Unroll);
 
     auto queue = currentQueue(0, 0);
     auto e = queue.submit([&](sycl::handler &cgh) {
@@ -44,13 +74,18 @@ struct copy_persist {
 
 size_t parse_nelems(const std::string& nelems_string) {
   size_t base = 1;
-  size_t pos = nelems_string.rfind("M");
+  size_t pos = nelems_string.rfind("K");
   if (pos != std::string::npos) {
-    base = 1024 * 1024ull;
+    base = 1024ull;
   } else {
-    pos = nelems_string.rfind("G");
+    pos = nelems_string.rfind("M");
     if (pos != std::string::npos)
-      base = 1024 * 1024 * 1024ull;
+      base = 1024 * 1024ull;
+    else {
+      pos = nelems_string.rfind("G");
+      if (pos != std::string::npos)
+        base = 1024 * 1024 * 1024ull;
+    }
   }
 
   return stoull(nelems_string) * base;
@@ -73,7 +108,15 @@ double bandwidth_from_event(sycl::event e, size_t nelems) {
   auto end = e.template get_profiling_info<sycl::info::event_profiling::command_end>();
 
   // since timestamp is in the unit of ns, then the bandwidth is GB/s in unit
-  return (double)(nelems * sizeof(T)) / (double)(end - start);
+  return (double)(nelems * sizeof(T) * 2) / (double)(end - start);
+}
+
+double time_from_event(sycl::event e) {
+  e.wait();
+  auto start = e.template get_profiling_info<sycl::info::event_profiling::command_start>();
+  auto end = e.template get_profiling_info<sycl::info::event_profiling::command_end>();
+
+  return (double)(end -start);
 }
 
 int main(int argc, char *argv[]) {
@@ -81,10 +124,17 @@ int main(int argc, char *argv[]) {
   opts.allow_unrecognised_options();
   opts.add_options()
     ("n,nelems", "Number of elements", cxxopts::value<std::string>()->default_value("16MB"))
+    ("u,unroll", "Unroll request", cxxopts::value<uint32_t>()->default_value("1"))
+    ("g,groups", "Max Group Size", cxxopts::value<size_t>()->default_value("64"))
+    ("l,local", "Local size", cxxopts::value<size_t>()->default_value("512"))
     ;
 
   auto parsed_opts = opts.parse(argc, argv);
   auto nelems_string = parsed_opts["nelems"].as<std::string>();
+  auto unroll =  parsed_opts["unroll"].as<uint32_t>();
+  auto local = parsed_opts["local"].as<size_t>();
+  auto max_groups = parsed_opts["groups"].as<size_t>();
+
   auto nelems = parse_nelems(nelems_string);
   using test_type = sycl::half;
   constexpr uint32_t v_lane = 16;
@@ -94,13 +144,38 @@ int main(int argc, char *argv[]) {
 
   auto* src = (test_type *)sycl::malloc_device(alloc_size, queue);
   auto* dst = (test_type *)sycl::malloc_device(alloc_size, queue);
-  auto* b_host = sycl::malloc_device(alloc_size, queue);
+  auto* b_host = sycl::malloc_host(alloc_size, queue);
   fill_sequential<test_type>(b_host, 0, alloc_size);
 
   queue.memcpy(src, b_host, alloc_size);
   queue.wait();
 
-  auto e = copy_persist<test_type, v_lane>::launch(dst, src, nelems);
+  sycl::event e;
+  switch (unroll) {
+    case 1:
+      e = copy_persist<test_type, v_lane, 1, jump_copy>::launch(dst, src, nelems, max_groups, local);
+      break;
+    case 2:
+      e = copy_persist<test_type, v_lane, 2, jump_copy>::launch(dst, src, nelems, max_groups, local);
+      break;
+    case 4:
+      e = copy_persist<test_type, v_lane, 4, jump_copy>::launch(dst, src, nelems, max_groups, local);
+      break;
+    case 8:
+      e = copy_persist<test_type, v_lane, 8, jump_copy>::launch(dst, src, nelems, max_groups, local);
+      break;
+    default:
+      throw std::logic_error("Unroll request not supported");
+  }
+  // auto e = queue.memcpy(dst, src, alloc_size);
   auto bandwidth = bandwidth_from_event<test_type>(e, nelems);
-  printf("Copy persistent kernel bandwidth: %fGB/s\n", bandwidth);
+  auto time = time_from_event(e);
+  printf("Copy %zu half in %fns, bandwidth: %fGB/s\n", alloc_size, time, bandwidth);
+
+  auto* b_check = sycl::malloc_host(alloc_size, queue);
+  queue.memcpy(b_check, dst, alloc_size);
+  queue.wait();
+
+  if (memcmp(b_check, b_host, alloc_size) == 0)
+    printf("Verified\n");
 }
