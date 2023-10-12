aaf50f327e610cd226e6eb694801548b4b2cc709
Cao, Zhong Z <zhong.z.cao@intel.com>
2023-10-12 05:06:43 -0700

    Change script
    Signed-off-by: Cao, Zhong Z <zhong.z.cao@intel.com>

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
+export FI_PROVIDER=tcp
 export ONEAPI_DEVICE_SELECTOR=level_zero:gpu
-export PATH=/home/caozhong/Workspace/ccl/release/_install/bin:$PATH
-export LD_LIBRARY_PATH=/home/caozhong/Workspace/ccl/release/_install/lib:$LD_LIBRARY_PATH
-export NEOReadDebugKeys=1
-export SetCommandStreamReceiver=3
-export ProductFamilyOverride=pvc
+# export PATH=/home/caozhong/Workspace/ccl/release/_install/bin:$PATH
+# export LD_LIBRARY_PATH=/home/caozhong/Workspace/ccl/release/_install/lib:$LD_LIBRARY_PATH
 
-mpirun -np 8 ./fill_remote -c 16 -t fp16
+mpirun -np 8 ./fill_remote $@
