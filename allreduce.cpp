d2edcacdc1a016c2e158de63e014655decf0e6b7
Cao Zhong Z <zhong.z.cao@intel.com>
2024-02-23 12:33:25 +0800

    Add test for another level of parallel
    Signed-off-by: Cao Zhong Z <zhong.z.cao@intel.com>

--- allreduce.cpp
+++ allreduce.cpp
@@ -907,7 +907,7 @@ template <> sycl::event testTransmit <sycl::half, bisectPTransmit> (
   if (subgroup == 16) {
     constexpr int SubGroupSize = 16;
 #if defined(__fix_param_passing__) && !defined(__enable_sycl_stream__)
-    bisectPAllReduce<sycl::half, 8, bisectPTransmit, SubGroupSize>
+    bisectPPAllReduce<sycl::half, 8, bisectPTransmit, SubGroupSize>
       devOp(input, nelems, rank, step, ipcbuf0, ipcbuf1, peerbuf0, peerbuf1);
     sycl::buffer params(
       const_cast<const decltype(devOp) *>(&devOp),
@@ -931,7 +931,7 @@ template <> sycl::event testTransmit <sycl::half, bisectPTransmit> (
 #endif
         cgh.parallel_for(
           launchParam,
-          bisectPAllReduce<sycl::half, 8, bisectPTransmit, SubGroupSize>(
+          bisectPPAllReduce<sycl::half, 8, bisectPTransmit, SubGroupSize>(
             input, nelems, rank, step,
             ipcbuf0, ipcbuf1, peerbuf0, peerbuf1
 #if defined(__enable_sycl_stream__)
@@ -952,7 +952,7 @@ template <> sycl::event testTransmit <sycl::half, bisectPTransmit> (
 #endif
         cgh.parallel_for(
           launchParam,
-          bisectPAllReduce<sycl::half, 8, bisectPTransmit, SubGroupSize>(
+          bisectPPAllReduce<sycl::half, 8, bisectPTransmit, SubGroupSize>(
             input, nelems, rank, step,
             ipcbuf0, ipcbuf1, peerbuf0, peerbuf1
 #if defined(__enable_sycl_stream__)
