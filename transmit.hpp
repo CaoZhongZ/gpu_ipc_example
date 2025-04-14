3b6375c7527076121122c5843e7dc90644f84a77
Cao Zhong Z <zhong.z.cao@intel.com>
2025-04-14 20:49:10 +0800

    Correct macro
    Signed-off-by: Cao Zhong Z <zhong.z.cao@intel.com>

--- transmit.hpp
+++ transmit.hpp
@@ -4,10 +4,10 @@
 #include <gen_visa_templates.hpp>
 
 #define divUp(x, m)  \
-  ((x + m -1) / m)
+  (((x) + (m) -1) / (m))
 
 #define alignUp(x, c) \
-  (divUp(x, c) * c)
+  (divUp((x), (c)) * (c))
 
 #include "rt64.hpp"
 #include "rt64_128.hpp"
